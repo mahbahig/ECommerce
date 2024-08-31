dc3c84ce443cf16c9d7b1d43dce5e0d550357de2
mahbahig <mahbahig2@gmail.com>
2024-08-31 03:19:50 +0300

    Fixed api and reposnive bugs

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,10 @@ export class AuthService {
   private readonly _HttpClient = inject(HttpClient);
 
   setRegisterForm(data: Object):  Observable<any> {
-    return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signup', data);
+    return this._HttpClient.post(`${environment.baseUrl}/api/v1/auth/signup`, data);
   }
 
   setLoginForm(data: Object): Observable<any> {
-    return this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signin', data);
+    return this._HttpClient.post(`${environment.baseUrl}/api/v1/auth/signin`, data);
   }
 }
